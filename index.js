0f75e8287a5fed760ddd66f1248b12dbcc96c8f7
Jacob Wright <mee@mailinator.com>
2017-12-16 22:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function updateObjectWithKeyAndValue(object,key,value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
 }
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+}
